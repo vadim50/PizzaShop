a62684a61e70557e736d39cd0973edd8bbc582ec
vadim <you@example.com>
2017-04-14 16:07:24 +0300

    Update add_to_cart function

--- public/hello.js
+++ public/hello.js
@@ -1,7 +1,10 @@
 
 function add_to_cart(id)
 {
-	alert('You added pizza with id: ' + id);
+	var key = 'product' + id;
+	var x = window.localStorage.getItem(key);
+	x = x * 1 + 1;
+	window.localStorage.setItem(key, x)
 }
 function someThing()
 	{
