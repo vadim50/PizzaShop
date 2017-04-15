ce92f8dc4289b4ab184150f611757a3f1d9654fa
vadim <you@example.com>
2017-04-15 11:52:36 +0300

    Add function get_cart

--- public/hello.js
+++ public/hello.js
@@ -1,11 +1,22 @@
 
 function add_to_cart(id)
 {
-	var key = 'product' + id;
+	var total = 0;
+	var key = 'product_' + id;
 	var x = window.localStorage.getItem(key);
 	x = x * 1 + 1;
-	window.localStorage.setItem(key, x)
+	window.localStorage.setItem(key, x);
+
+	for ( var i = 0, len = localStorage.length; i < len; ++i )
+	{
+    	y = localStorage.getItem( localStorage.key( i ) ) ;
+   		total = total  + y * 1;
+	}
+
+	document.getElementById("cart").innerHTML = "Total: " + total;
 }
+
+
 function someThing()
 	{
 		var x = window.localStorage.getItem('s');
@@ -16,6 +27,21 @@ function someThing()
 	
 	}
 
+function cart_get()
+{
+		var cnt = 0;
+	for(var i = 0 ; i < window.localStorage.length; i++)
+	{
+		var key = window.localStorage.key(i);
+		var value = window.localStorage.getItem(key);
+
+		if(key.indexOf('product_') == 0)
+		{
+			cnt = cnt + value * 1;
+		}
+	}
+	return cnt;
+}
 
 
 
