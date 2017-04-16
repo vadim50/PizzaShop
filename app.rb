63b7263c99e8491fc2699cb8061e9abc5ca480aa
vadim <you@example.com>
2017-04-16 16:51:52 +0300

    Add cart to page

--- app.rb
+++ app.rb
@@ -15,37 +15,42 @@ get '/' do
 end
 
 get '/about' do
+	@product = Product.find(1)
 
 	erb :about
 end
 
 
 post '/cart' do
-	orders = params[:orders]
-	@orders = parse_orders_input orders
-  erb :cart
+  @orders_input = params[:orderstring]
+  @items = parse_orders_input @orders_input
+
+  @items.each do |item|
+    #id, cnt
+    item[0] = Product.find(item[0])
+  end
+	erb :cart
 end
 
-def parse_orders_input orders
+def parse_orders_input orders_input
+  s1 = orders_input.split(/,/)
 
-	s1 = orders.split(/,/)
+  arr = []
 
-	arr = []
+  s1.each do |x|
+    s2 = x.split(/\=/)
 
-	s1.each do |x|
-		s2 = x.split(/=/)
-		s3 = s2[0].split(/_/)
+    s3 = s2[0].split(/_/)
 
-		id = s3[1]
-		cnt = s2[1]
-		arr2 = [id, cnt]
+    id = s3[1]
+    cnt = s2[1]
 
-		arr.push arr2
+    arr2 = [id, cnt]
 
-	end
+    arr.push arr2
+  end
 
-	return arr
-
+  return arr
 end
 
 
--- db/migrate/20170413142401_agg_products.rb
+++ db/migrate/20170413142401_agg_products.rb
@@ -3,7 +3,7 @@ class AggProducts < ActiveRecord::Migration[5.0]
   	Product.create :title => 'Hawaiian', :description => 'This is Hawaiian pizza',:price => 350,:size => 30, :is_spicy => false,:is_veg => false, :is_best_offer => false, :path_to_image => '/images/hawaiiian.jpg'
   	
 
-  	Product.create :title => 'Pepperoni', :description => 'This is Pepperoni pizza',:price => 450,:size => 30, :is_spicy => false,:is_veg => false, :is_best_offer => true, :path_to_image => '/images/pepperoni.jpg'
+  	Product.create :title => 'Pepperoni2', :description => 'This is Pepperoni pizza',:price => 450,:size => 30, :is_spicy => false,:is_veg => false, :is_best_offer => true, :path_to_image => '/images/pepperoni.jpg'
   	
   	
   	
