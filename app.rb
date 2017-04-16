349210732168b5dad1d76819d1d9a774cc71588e
vadim <you@example.com>
2017-04-16 13:28:03 +0300

    Parse orders_input and add view cart

--- app.rb
+++ app.rb
@@ -15,15 +15,38 @@ get '/' do
 end
 
 get '/about' do
+
 	erb :about
 end
 
 
 post '/cart' do
-  erb "Hello World"
+	orders = params[:orders]
+	@orders = parse_orders_input orders
+  erb :cart
 end
 
+def parse_orders_input orders
 
+	s1 = orders.split(/,/)
+
+	arr = []
+
+	s1.each do |x|
+		s2 = x.split(/=/)
+		s3 = s2[0].split(/_/)
+
+		id = s3[1]
+		cnt = s2[1]
+		arr2 = [id, cnt]
+
+		arr.push arr2
+
+	end
+
+	return arr
+
+end
 
 
 
